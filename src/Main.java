ca344ede07c4a1ae118b6bf62d3e8c5cdd7b7257
Николай Тумаркин <nickolai.tumarkin@yandex.ru>
2025-01-22 20:47:45 +0300

    Добавил мини план

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        //main
+        //рандомно перемешать по секциям
+        //создание 9 объектов людей
+        //HashMap <String, ArrayList<Person>>
+        //
+        //Person(){
+        //name
+        //age
+        //weight
+        //id
+        //}
+        //getid() {
+        //}
+        //
+        //Methods
+        //1) signIn (for () {
+        //if (бассейн...) {}
+        //else if (йога) {}
+        //else if (бокс)
+        //})
+        //2) start
+        //3) finish
 
     }
 }
