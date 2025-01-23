4a596e1912da812df746b7504f4ccfdbca091b94
Pitentii Gleb <pitsentiy@inbox.ru>
2025-01-23 15:44:26 +0300

    индуйский код)

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,53 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+        // Создаем объекты секций
+        Activities.YogaSection yogaSection = new Activities.YogaSection();
+        Activities.BoxSection boxSection = new Activities.BoxSection();
+        Activities.PoolSection poolSection = new Activities.PoolSection();
 
-        //main
-        //рандомно перемешать по секциям
-        //создание 9 объектов людей
-        //HashMap <String, ArrayList<Person>>
-        //
-        //Person(){
-        //name
-        //age
-        //weight
-        //id
-        //}
-        //getid() {
-        //}
-        //
-        //Methods
-        //1) signIn (for () {
-        //if (бассейн...) {}
-        //else if (йога) {}
-        //else if (бокс)
-        //})
-        //2) start
-        //3) finish
+        // Создаем ArrayList
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(new Person("Анна", 25, 65.0));
+        people.add(new Person("Борис", 30, 80.0));
+        people.add(new Person("Вера", 28, 58.0));
+        people.add(new Person("Григорий", 35, 90.0));
+        people.add(new Person("Дарья", 22, 55.0));
+        people.add(new Person("Евгений", 40, 85.0));
+        people.add(new Person("Жанна", 27, 62.0));
+        people.add(new Person("Захар", 33, 78.0));
+        people.add(new Person("Ирина", 29, 60.0));
 
+        // Создаем HashMap
+        HashMap<String, ArrayList<Person>> sections = new HashMap<>();
+        sections.put("Йога", new ArrayList<>());
+        sections.put("Бокс", new ArrayList<>());
+        sections.put("Бассейн", new ArrayList<>());
 
+        // Распределяем людей по секциям и проводим тренировки
+        Activities2 activities = new Activities2();
+        
+        for (Person person : people) {
+            // Распределяем человека в соответствующий список
+            switch (person.getId()) {
+                case 1 -> sections.get("Йога").add(person);
+                case 2 -> sections.get("Бокс").add(person);
+                case 3 -> sections.get("Бассейн").add(person);
+            }
+            // Проводим тренировку
+            activities.SectionDecider(person);
+        }
+
+        // Вывод
+        System.out.println("\nИтоговое распределение по секциям:");
+        for (String section : sections.keySet()) {
+            System.out.println("\n" + section + ":");
+            for (Person person : sections.get(section)) {
+                System.out.println(person.getName() + " - Вес после тренировки: " + person.getWeight() + " кг");
+            }
+        }
     }
 }
